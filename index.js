0efb1027a30297b8102de5b029e809b5f593b8e5
Paulo Santiago <pauloecsantiago@gmail.com>
2023-08-15 18:15:44 +0100

    Render dog on page

--- index.js
+++ index.js
@@ -2,3 +2,11 @@
 
 import data from "./data.js";
 import Dog from "./Dog.js";
+
+function render() {
+  // @ts-ignore
+  document.getElementById("outsider-div").innerHTML = dog.getNewDogHtml();
+}
+
+let dog = new Dog(data[0]);
+render();
