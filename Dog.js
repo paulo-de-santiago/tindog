9266b29fb58eb76c5da295f5757e47410e60da7b
Paulo Santiago <pauloecsantiago@gmail.com>
2023-08-16 11:30:58 +0100

    Target data with dataet

--- Dog.js
+++ Dog.js
@@ -10,29 +10,33 @@ class Dog {
     const { name, avatar, age, bio } = this;
 
     return `<div class="outsider-div" id="outsider-div">
-    <img src="${avatar}" alt="" />
+              <img src="${avatar}" alt="" />
 
-    <div class="dog-info-general">
-      <div class="badge-like" id="badge-like">
-        <img src="./images/badge-like.png" alt="<!--  -->" class="badge-like-img" id="badge-like-img"/>
-      </div>
-      <div class="dog-info" id="dog-info">
-        <h3>${name}, ${age}</h3>
-        <p>${bio}</p>
-      </div>
-    </div>
-  </div>
+                <div class="dog-info-general">
+                    <div class="badge-like" id="badge-like">
+                      <img src="./images/badge-like.png" alt="<!--  -->" class="badge-like-img" id="badge-like-img"/>
+                    </div>
+                    <div class="dog-info" id="dog-info">
+                      <h3>${name}, ${age}</h3>
+                      <p>${bio}</p>
+                    </div>
+                </div>
+            </div>
 
-  <footer class="footer-icons" id="footer-icons">
-    <div class=div-footer-icons id="div-footer-icons">
-      <button class="no" id="no" data-no="${name}">
-        <img src="./images/icon-cross.png" alt="No" class="no-size" id="no-size"/>
-      </button>
-      <button class="like" id="like" data-like="${name}">
-        <img src="./images/icon-heart.png" alt="Like" class="like-size" id="like-size"/>
-      </button>
-     
-    </div>`;
+            <footer class="footer-icons" id="footer-icons">
+              <div class=div-footer-icons id="div-footer-icons">
+
+                  <button class="no" id="no" data-no="${name}">
+                    <img src="./images/icon-cross.png" alt="No" class="no-size" id="no-size"/>
+                  </button>
+
+                  <button class="like" id="like" data-like="${name}">
+                    <img src="./images/icon-heart.png" alt="Like" class="like-size" id="like-size"/>
+                  </button>  
+
+              </div>
+            </footer>    
+            `;
   }
 }
 
--- index.js
+++ index.js
@@ -1,8 +1,23 @@
+// @ts-nocheck
 // Remember to import the data and Dog class!
 
 import data from "./data.js";
 import Dog from "./Dog.js";
 
+document.addEventListener("click", function (element) {
+  // @ts-ignore
+  if (element.target.dataset.no) {
+    render();
+  } else if (element.target.dataset.like) {
+    render();
+  }
+});
+
+/* function buttonNo(data) {
+  document.getElementById("no");
+  console.log(data);
+}
+ */
 function render() {
   // @ts-ignore
   document.getElementById("outsider-div").innerHTML = dog.getNewDogHtml();
