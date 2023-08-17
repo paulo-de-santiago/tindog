9c800fdc5603c56ec8f661c986a084bf23b05581
Paulo Santiago <pauloecsantiago@gmail.com>
2023-08-17 11:39:55 +0100

    Nope btn resized

--- index.js
+++ index.js
@@ -24,16 +24,17 @@ let dog = new Dog(dogsData[counting]);
 function buttonOption(option) {
   let btnNope = document.getElementById("badge-nope-img").style;
 
-  let btnLike = document.getElementById("badge-nope-img").style;
+  let btnLike = document.getElementById("badge-like-img").style;
 
-  if (option === "no") {
+  if (option === "nope") {
     btnNope.display = "inline";
     setTimeout(getNewDog, 5000);
     dog.setStatus(false);
-    console.log("no");
+    console.log("nope");
   } else if (option === "like") {
     btnLike.display = "inline";
     dog.setStatus(true);
+    setTimeout(getNewDog, 2000);
     console.log("like");
   }
   /*   getNewDog(); */
