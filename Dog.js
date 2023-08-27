5f260686b33f8a98f2598ba4587202be110f7d2b
Paulo Santiago <pauloecsantiago@gmail.com>
2023-08-27 14:31:42 +0100

    Dog class deleted div

--- Dog.js
+++ Dog.js
@@ -17,8 +17,7 @@ image.src = "image1.jpg" */
     // @ts-ignore
     const { name, avatar, age, bio } = this;
 
-    return `<div class="outsider-div" id="outsider-div">
-              <img src="${avatar}" alt=""/>
+    return ` <img src="${avatar}" alt=""/>
                 <div class="dog-info-general">
                     <div class="badge" id="badge">
                       <img src="./images/badge-nope.png" alt="" class="badge-nope-img" id="badge-nope-img"/> 
@@ -29,9 +28,6 @@ image.src = "image1.jpg" */
                       <p>${bio}</p>
                     </div>
                 </div>
-            </div>
-
-        
             `;
   }
 }
